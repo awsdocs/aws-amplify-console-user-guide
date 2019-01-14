10ffbeb14f42d0cd42b596e9ab1f5a639d774f88
Jacob Warren <jacowarr@amazon.com>
2019-01-14 21:39:55 +0000

    Updated conf.py so that the doc package can be built with Sphinx.

--- doc_source/conf.py
+++ doc_source/conf.py
@@ -11,8 +11,7 @@
 # General information about the project.
 #
 
-# The long version of the service or SDK name, such as "Amazon Simple Workflow
-# Service", "AWS Flow Framework for Ruby" or "AWS SDK for Java"
+# The long version of the service or SDK name
 service_name_long = u'AWS Amplify'
 service_docs_home = u'http://aws.amazon.com/documentation/amplify/'
 
@@ -44,5 +43,35 @@ extra_navlinks = []
 # The link to the top of the doc source tree on GitHub. This allows generation
 # of per-page "Edit on GitHub" links.
 github_doc_url = 'https://github.com/awsdocs/aws-amplify-console-user-guide/tree/master/doc_source'
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = ['sphinx.ext.extlinks']
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+
+# The master toctree document.
+master_doc = 'index'
+
+# -- Options for HTML output ----------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+html_theme = 'bizstyle'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = 'AWSAmplifyConsoleUserGuide'
 
 # EXTRA_CONF_CONTENT -- don't change, move or remove this line!
